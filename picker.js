0f0bf295890a3810f97721af8fc4dba9bb30964e
chapery <chenanping@mucang.cn>
2020-08-02 22:53:50 +0800

    toggle

--- picker.js
+++ picker.js
@@ -1,28 +1,47 @@
 var Picker = function (target, config) {
     this.elTarget = typeof target === 'string' ? document.querySelector(target) : target;
     this.config = config;
-    this.elPicker = document.querySelector('.picker-container');
-    this.elColumns = this.elPicker.querySelector('.picker-columns');
-    this.touch = {
-        elScroll: null,
-        elList: null,
-        startY: 0,
-        translateY: 0,
-        /**
-         * 是否正在进行惯性滚动
-         */
-        inertiaActive: false,
-        /**
-         * 最后五次touchmove点
-         * clientY
-         * timeStamp
-         */
-        lastTouchMovePoints: []
-    },
+    this.elPicker = document.querySelector('.picker-mask');
+    this.elColumnsWrap = this.elPicker.querySelector('.picker-columns');
+
     // 允许超出滚动边界的最大距离
-    this.flexableDistance = 80;
-    
+    this.flexableDistance = 100;
+
+    // 存放每列相关属性
+    this.columns = this.config.columns.map(function () {
+
+        return {
+            elColumn: null,
+            elScroll: null,
+            elList: null,
+            elItems: null,
+            // 是否处于触控状态
+            touchActive: false,
+            startY: 0,
+            translateY: 0,
+            /**
+             * 是否正在进行惯性滚动
+             */
+            inertiaActive: false,
+            /**
+             * 最后五次touchmove点
+             * clientY
+             * timeStamp
+             */
+            lastTouchMovePoints: [],
+            // 当前选中项索引
+            activeIndex: 0
+        }
+    });
+
+    // touch状态下的column
+    this.activeColumn = null;
+
+    // picker是否处于显示状态
+    this.active = false;
+
     this.createColumns();
+    this.layout();
     this.bindEvent();
 }
 
@@ -31,63 +50,125 @@ Picker.prototype = {
     consturctor: Picker,
 
     /**
+     * 显示或隐藏picker
+     * @param {Boolean} isShow 可选 是否显示，无此参数时取当前相反的状态
+     */
+    toggle: function (isShow) {
+        var that = this;
+
+        if (typeof isShow === 'undefined') {
+            isShow = !this.active;
+        }
+
+        if (isShow) {
+            this.elPicker.classList.add('active');
+            this.layout();
+            setTimeout(function () {
+                that.elPicker.classList.add('transition');
+            }, 0);
+        } else {
+            this.elPicker.classList.remove('transition');
+            setTimeout(function () {
+                that.elPicker.classList.remove('active');
+            }, 300);
+        }
+
+        this.active = isShow;
+    },
+
+    /**
      * 生成列
      */
     createColumns: function () {
         var that = this;
 
-        that.config.columns.forEach(function (column) {
+        that.config.columns.forEach(function (column, columnIndex) {
+            var activeColumn = that.columns[columnIndex];
             var elColumn = document.createElement('div');
-            var optionsHtml = column.options.map(function (item) {
+            var elScroll = null;
+            var elList = null;
+            var elItems = null;
+
+            var itemsHtml = column.options.map(function (item) {
                 return '<div class="picker-item">' + item.name + '</div>';
             }).join('');
-            var columnHtml = '<div class="picker-scroll">\
+            var columnHtml = '<div class="picker-scroll" data-column-index="' + columnIndex + '">\
                                 <div class="picker-list">'
-                                + optionsHtml +
-                                '</div>\
+                + itemsHtml +
+                '</div>\
                             </div>\
                             <div class="picker-label">'
-                            + column.label +
-                            '</div>';
+                + column.label +
+                '</div>';
 
             elColumn.className = 'picker-column';
             elColumn.innerHTML = columnHtml;
 
-            that.elColumns.appendChild(elColumn);
+            that.elColumnsWrap.appendChild(elColumn);
 
+            elScroll = elColumn.querySelector('.picker-scroll');
+            elList = elScroll.querySelector('.picker-list');
+            elItems = [].slice.call(elList.querySelectorAll('.picker-item'));
+
+            activeColumn.elColumn = elColumn;
+            activeColumn.elScroll = elScroll;
+            activeColumn.elList = elList;
+            activeColumn.elItems = elItems;
         });
-    },
 
+    },
+    
     bindEvent: function () {
         var that = this;
+        
+        this.elTarget.addEventListener('click', this.toggle.bind(this), false);
+        this.elPicker.addEventListener('click', function (e) {
+            if (e.currentTarget === e.target) {
+                that.toggle();
+            }
+        }, false);
 
-        [].forEach.call(that.elColumns.querySelectorAll('.picker-scroll'), function (elScroll) {
-            elScroll.addEventListener('touchstart', that.touchStart.bind(that), false);
-            elScroll.addEventListener('touchend', that.touchEnd.bind(that), false);
+        this.elPicker.querySelector('.picker-cancel').addEventListener('click', function () {
+            that.toggle();
+        }, false);
+        
+        this.columns.forEach(function (column) {
+            column.elScroll.addEventListener('touchstart', that.touchStart.bind(that), false);
+            column.elScroll.addEventListener('touchend', that.touchEnd.bind(that), false);
         });
 
         document.addEventListener('touchmove', that.touchMove.bind(that), false);
     },
 
+    layout: function () {
+        this.columns.forEach(function (column) {
+            column.elList.style.top = (column.elScroll.clientHeight - column.elItems[0].clientHeight) / 2 + 'px';
+        });
+    },
+
     touchStart: function (e) {
-        this.touch.startY = e.touches[0].clientY;
-        this.touch.elScroll = e.currentTarget;
-        this.touch.elList = e.currentTarget.querySelector('.picker-list');
-        this.touch.inertiaActive = false;
+        var activeColumn = this.columns[e.currentTarget.dataset.columnIndex];
+
+        activeColumn.startY = e.touches[0].clientY;
+        activeColumn.touchActive = true;
+        activeColumn.inertiaActive = false;
+
+        this.activeColumn = activeColumn;
+
         e.preventDefault();
     },
 
     touchMove: function (e) {
+        var activeColumn = this.activeColumn;
         var disY = 0;
         var clientY = e.touches[0].clientY;
-        var elItems = this.touch.elList.querySelectorAll('.picker-item');
-        var disYMin = -elItems[0].clientHeight * (elItems.length - 1);
+        var disYMin = -activeColumn.elItems[0].clientHeight * (activeColumn.elItems.length - 1);
 
-        if (!this.touch.elScroll) {
+        if (!activeColumn.touchActive) {
             return true;
         }
 
-        disY = clientY - this.touch.startY + (Number(this.touch.elList.dataset.translateY) || 0);
+        disY = clientY - activeColumn.startY + (Number(activeColumn.elList.dataset.translateY) || 0);
 
         // 超过上下边界
         if (disY > 0) {
@@ -96,39 +177,36 @@ Picker.prototype = {
             disY = -(this.flexableDistance / Math.sqrt(screen.height) * Math.sqrt(-disY + disYMin)) + disYMin;
         }
 
-        this.touch.elList.style.transform = 'translate(0, ' + disY + 'px)';
-        this.touch.translateY = disY;
-        this.touch.lastTouchMovePoints.push({
+        activeColumn.elList.style.transform = 'translate(0, ' + disY + 'px)';
+        activeColumn.translateY = disY;
+        activeColumn.lastTouchMovePoints.push({
             clientY: clientY,
             timeStamp: e.timeStamp
         });
-        this.touch.lastTouchMovePoints = this.touch.lastTouchMovePoints.slice(-5);
+        activeColumn.lastTouchMovePoints = activeColumn.lastTouchMovePoints.slice(-5);
     },
 
     touchEnd: function (e) {
-        var that = this;
-        var elItemHeight = 0;
+        var activeColumn = this.activeColumn;
         var speed = 0;
         var touchMovePointLast = null;
 
-        if (!that.touch.elScroll) {
+        if (!activeColumn.touchActive) {
             return true;
         }
 
-        elItemHeight = that.touch.elList.children[0].clientHeight;
-
-        if (that.touch.lastTouchMovePoints.length >= 2) {
-            touchMovePointLast = that.touch.lastTouchMovePoints[that.touch.lastTouchMovePoints.length - 1];
-            speed = (touchMovePointLast.clientY - that.touch.lastTouchMovePoints[0].clientY) / (touchMovePointLast.timeStamp - that.touch.lastTouchMovePoints[0].timeStamp);
+        if (activeColumn.lastTouchMovePoints.length >= 2) {
+            touchMovePointLast = activeColumn.lastTouchMovePoints[activeColumn.lastTouchMovePoints.length - 1];
+            speed = (touchMovePointLast.clientY - activeColumn.lastTouchMovePoints[0].clientY) / (touchMovePointLast.timeStamp - activeColumn.lastTouchMovePoints[0].timeStamp);
         } else {
             speed = 0;
         }
 
-        that.inertia(speed);
+        this.inertia(speed);
 
-        that.touch.elScroll = null;
-        that.touch.startY = 0;
-        that.touch.lastTouchMovePoints = [];
+        activeColumn.touchActive = false;
+        activeColumn.startY = 0;
+        activeColumn.lastTouchMovePoints = [];
     },
 
     /**
@@ -137,11 +215,12 @@ Picker.prototype = {
      */
     inertia: function (speed) {
         var that = this;
+        var activeColumn = this.activeColumn;
         var requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
         // 最小速度
         var speedMin = 0.3;
         // 最大速度
-        var speedMax = 2 ;
+        var speedMax = 2;
         // 速度放大系数
         var speedFactor = 10;
         var duration = 1000 / 60;
@@ -155,20 +234,19 @@ Picker.prototype = {
         var willDistance = 0;
         // 总帧数
         var frameCount = 0;
-        var elItems = this.touch.elList.querySelectorAll('.picker-item');
         // 选项高度
-        var optionHeight = elItems[0].clientHeight;
+        var itemHeight = activeColumn.elItems[0].clientHeight;
         // 下边界位移
-        var disYMin = -optionHeight * (elItems.length - 1);
+        var disYMin = -itemHeight * (activeColumn.elItems.length - 1);
         // 实际移动总距离
         var realDistance = 0;
         // 每一帧执行
         var processFn = function () {
             var translateY = 0;
 
-            translateY = that.touch.translateY + distance;
-            that.touch.elList.style.transform = 'translate(0, ' + translateY + 'px)';
-            that.touch.elList.dataset.translateY = translateY;
+            translateY = activeColumn.translateY + distance;
+            activeColumn.elList.style.transform = 'translate(0, ' + translateY + 'px)';
+            activeColumn.elList.dataset.translateY = translateY;
         };
 
         // 保证速度不为0
@@ -178,7 +256,7 @@ Picker.prototype = {
         acceleration *= -Math.sign(speed);
 
         if (!requestAnimationFrame) {
-            requestAnimationFrame = function( callback ){
+            requestAnimationFrame = function (callback) {
                 window.setTimeout(callback, duration);
             };
         }
@@ -189,30 +267,30 @@ Picker.prototype = {
         speed *= speedFactor;
 
         frameCount = Math.ceil(Math.abs(speed / acceleration));
-        
+
         willDistance = frameCount * (speed + (frameCount - 1) * acceleration / 2);
 
-        realDistance = Math.round((willDistance + that.touch.translateY) / optionHeight) * optionHeight - that.touch.translateY;
+        realDistance = Math.round((willDistance + activeColumn.translateY) / itemHeight) * itemHeight - activeColumn.translateY;
 
         // 超过上下边界
-        if (willDistance + that.touch.translateY > 0) {
-            realDistance = -that.touch.translateY;
-        } else if (willDistance + that.touch.translateY < disYMin) {
-            realDistance = disYMin - that.touch.translateY;
+        if (willDistance + activeColumn.translateY > 0) {
+            realDistance = -activeColumn.translateY;
+        } else if (willDistance + activeColumn.translateY < disYMin) {
+            realDistance = disYMin - activeColumn.translateY;
         }
 
         if (Math.sign(realDistance) * Math.sign(willDistance) === -1) {
             speed = -speed;
         }
-        
+
         // 限制最小速度
         speed = Math.abs(speed) < (speedMin * speedFactor) ? Math.sign(speed) * (speedMin * speedFactor) : speed;
 
         // 重新计算加速度
         acceleration = Math.pow(speed, 2) / (speed - 2 * realDistance);
-        
+
         interval = function (callback) {
-            if (that.touch.inertiaActive && Math.sign(acceleration) * Math.sign(speed) === -1) {
+            if (activeColumn.inertiaActive && Math.sign(acceleration) * Math.sign(speed) === -1) {
                 // 减速中
                 requestAnimationFrame(function () {
                     callback();
@@ -221,12 +299,14 @@ Picker.prototype = {
             } else {
                 // 减速结束
                 processFn();
-                that.touch.translateY += distance;
-                that.touch.inertiaActive = false;
+                activeColumn.translateY += distance;
+                activeColumn.inertiaActive = false;
+
+                activeColumn.activeIndex = Math.round(Math.abs(activeColumn.translateY) / itemHeight);
             }
         };
 
-        that.touch.inertiaActive = true;
+        activeColumn.inertiaActive = true;
 
         interval(function () {
             distance += speed;
@@ -235,6 +315,10 @@ Picker.prototype = {
         });
     },
 };
+
+var Datetime = function () {
+
+}
 
 new Picker('input[data-picker]', {
     columns: [
